def8756dabd4b8a1b5fd0dcab5ecec3a9e3f9918
Alexia Lewis <lewisalexia208@gmail.com>
2023-03-09 16:41:48 -0600

    cleaning up whitespace

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -12,10 +12,9 @@ from employees
 where hire_date like '199%'
 	and birth_date like '%-12-25'
 order by hire_date
-limit 5 offset 50
+limit 5 offset 45
 ;
-
 -- Alselm Cappello, Utz Mandell, Bouchung Schreiter, Baocai Kushner, Petter Stroustrup
 
 -- The relationship between limit, offset, and page number is that you can multiply limit and page number to
--- find your offset supposing you want the same amount of results on each page
+-- find your offset supposing you want the same amount of results on each page and then subtract whatever your limit is
