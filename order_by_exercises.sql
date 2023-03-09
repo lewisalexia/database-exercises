71962ccb92ed35594aba57da0d2fb7840de074d1
Alexia Lewis <lewisalexia208@gmail.com>
2023-03-09 16:29:32 -0600

    cleaning up whitespace

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -132,7 +132,6 @@ select hire_date, birth_date, first_name, last_name
 from employees
 where hire_date like '199%'
 	and birth_date like '%-12-25'
-order by birth_date desc, hire_date 
+order by birth_date, hire_date desc
 ;
-
 -- 362, Khun Bernini and Douadi Pettis
