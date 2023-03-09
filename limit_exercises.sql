70552dbdb275a1676da812a103145b0f72252b40
Alexia Lewis <lewisalexia208@gmail.com>
2023-03-09 14:18:17 -0600

    turning in limit, order by, and where advanced exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,21 @@
+use employees;
+select database();
+
+select distinct last_name
+from employees
+order by last_name desc
+limit 10
+;
+
+select hire_date, birth_date, first_name, last_name
+from employees
+where hire_date like '199%'
+	and birth_date like '%-12-25'
+order by hire_date
+limit 5 offset 50
+;
+
+-- Alselm Cappello, Utz Mandell, Bouchung Schreiter, Baocai Kushner, Petter Stroustrup
+
+-- The relationship between limit, offset, and page number is that you can multiply limit and page number to
+-- find your offset supposing you want the same amount of results on each page
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,4 +1,4 @@
-use employees;
+-- WHERE ADVANCED EXERCISES
 select database();
 select *
 from employees
@@ -79,4 +79,60 @@ select distinct last_name
 from employees
 where last_name like '%q%'
 	and last_name not like '%qu%'
+
+-- ORDER BY EXERCISES
+use employees;
+select database();
+select *
+from employees
 ;
+
+select *
+from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+order by first_name
+;
+-- Irena Reutenauer and Vidya Simmen
+
+select *
+from employees
+where first_name = 'Irena'
+	or first_name = 'Vidya'
+	or first_name = 'Maya'
+order by first_name, last_name
+;
+-- Irena Acton and Vidya Zweizig
+
+select *
+from employees
+where first_name = 'Irena'
+	or first_name = 'Vidya'
+	or first_name = 'Maya'
+order by last_name, first_name
+;
+-- Irena Acton and Maya Zyda
+
+select first_name, last_name, emp_no
+from employees
+where last_name like 'E%'
+	and last_name like '%E'
+order by emp_no
+;
+-- 899, "10021 Ramzi Erde" and "499648 Tadahiro Erde"
+
+select first_name, last_name, hire_date
+from employees
+where last_name like 'E%'
+	and last_name like '%E'
+order by hire_date desc
+;
+-- 899, Teiji Eldridge and Sergi Erde
+
+select hire_date, birth_date, first_name, last_name
+from employees
+where hire_date like '199%'
+	and birth_date like '%-12-25'
+order by birth_date desc, hire_date 
+;
+
+-- 362, Khun Bernini and Douadi Pettis
