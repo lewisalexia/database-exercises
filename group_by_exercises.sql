60eee42bc1e71e262c0e3e06e99bc66557355930
Alexia Lewis <lewisalexia208@gmail.com>
2023-03-10 16:53:13 -0600

    cleaning up exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,23 +12,25 @@ from titles
 
 
 -- 3.
-select distinct last_name
+select last_name
 from employees
 where last_name like 'e%e'
 group by last_name
 ;
 
 -- 4.
-select distinct last_name, first_name
+select last_name, first_name
 from employees
 where last_name like 'e%e'
+group by last_name, first_name
 ;
 
 -- 5.
-select distinct last_name
+select last_name
 from employees
 where last_name like '%q%'
 	and last_name not like '%qu%'
+group by last_name
 ; -- Chleq, Lindqvist, Qiwen
 
 -- 6.
@@ -61,10 +63,11 @@ select
 from employees
 group by username
 order by count(*) desc
+-- having count(*) > 1 desc
 ;
 
--- 9 bonus | 27403
-select sum(duplicate)
+-- 9 bonus | sum = 27403 count = 13251
+select count(*)
 from (select
 	CONCAT(LOWER(SUBSTR(FIRST_NAME, 1, 1)),(LOWER(SUBSTR(LAST_NAME, 1, 4))),'_',SUBSTR(birth_date, 6,2),SUBSTR(birth_date, 3,2))
 		as username, count(*) as duplicate
@@ -74,6 +77,57 @@ having duplicate > 1)
 as total_duplicates
 ;
 
+-- bonus 1
+select emp_no, round(avg(salary),2)
+from salaries
+group by emp_no
+;
 
+-- bonus 2
+select dept_no, count(*) as current_dept_emp
+from dept_emp
+where to_date > now()
+group by dept_no
+;
 
+-- bonus 3
+select emp_no, count(*)
+from salaries
+group by emp_no
+order by count(*) desc
+;
+
+-- bonus 4
+select emp_no, max(salary)
+from salaries
+group by emp_no
+order by max(salary)
+;
+
+-- bonus 5
+select emp_no, min(salary)
+from salaries
+group by emp_no
+order by min(salary) 
+;
+
+-- bonus 6
+select emp_no, round(std(salary),1)
+from salaries
+group by emp_no
+;
+
+-- bonus 7
+select emp_no, max(salary)
+from salaries
+group by emp_no
+having max(salary) > 150000
+;
+
+-- bonus 8
+select emp_no, round(avg(salary), 2) as AVG_SALARY
+from salaries
+group by emp_no
+having avg_salary between 80000 and 90000
+;
 
