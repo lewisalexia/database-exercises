fc431d532f163a47467bd0a110e2130fe0cc52ac
Alexia Lewis <lewisalexia208@gmail.com>
2023-03-16 11:30:28 -0500

    adding case_exercises and temporary_tables exercises

--- case_exercises.sql
+++ case_exercises.sql
@@ -71,7 +71,6 @@ join employees e using (emp_no)) as p
 group by is_current_employee
 ; -- :)
 
-
 -- 2.
 -- Write a query that returns all employee names (previous and current),
 -- and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name.
